d1b65e7c52abb376862a5fad9bff475b995f9dbf
elusdota <elusjiangliang@189.cn>
2016-08-15 11:35:23 +0800

    EquipmentService

--- ffmp-core/src/main/java/com/jrtech/templates/services/EquipmentService.java
+++ ffmp-core/src/main/java/com/jrtech/templates/services/EquipmentService.java
@@ -1,7 +1,18 @@
 package com.jrtech.templates.services;
 
+import com.jrtech.ffmp.data.entities.Equipment;
+import com.jrtech.ffmp.data.entities.MaintenanceProject;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+
 /**
  * Created by jiangliang on 2016/8/15.
  */
 public interface EquipmentService {
+    public Page<Equipment> findByOwner(MaintenanceProject owner, Pageable pageable);
+
+    public Page<Equipment> findAll(Specification<Equipment> spec, Pageable pageable);
+
+    public Equipment findOneByCode(String code);
 }
