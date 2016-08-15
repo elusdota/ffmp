25cc288d448cdc4607077c42e5b528ed443eccec
suelmer <2532504569@qq.com>
2016-08-15 11:29:49 +0800

    EquipmentService

--- ffmp-core/src/main/java/com/jrtech/templates/services/EquipmentsERVICE.java
+++ ffmp-core/src/main/java/com/jrtech/templates/services/EquipmentsERVICE.java
@@ -4,10 +4,13 @@ import com.jrtech.ffmp.data.entities.Equipment;
 import com.jrtech.ffmp.data.entities.MaintenanceProject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 
 /**
  * Created by jiangliang on 2016/7/27.
  */
 public interface EquipmentService {
     Page<Equipment> findByOwner(MaintenanceProject owner, Pageable pageable);
+    Page<Equipment> findAll(Specification<Equipment> spec, Pageable pageable);
+    Equipment findOneByCode(String code);
 }
